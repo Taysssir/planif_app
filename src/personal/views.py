5383b2f77ac2d2a8e6c073a97ac79a59ca7d9ac5
Boubaker Tayssir <taysir.boubaker2082@gmail.com>
2022-12-05 13:59:01 +0100

    Structure Personal View

---
+++ src/personal/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_screen_view(request):
+	print(request.headers)
+	context = {}
+	# context['some_string'] = "this is some string that I'm passing to the view"
+	# context['some_number'] = 3151351
+
+	# context = {'some_string' :  "This is some string text passing from the view personal app",
+    # 'some_number' : 12334
+    # } 
+
+	list_of_values = []
+	list_of_values.append("first entry")
+	list_of_values.append("second entry")
+	list_of_values.append("third entry")
+	list_of_values.append("fourth entry")
+	list_of_values.append("fifth entry")
+	context['list_of_values'] = list_of_values
+
+	return render(request, "personal/home.html", context)
+	# return render(request, "base.html", {})
