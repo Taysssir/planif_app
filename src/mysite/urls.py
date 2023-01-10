5ef2f4065ae059120499c5bc7503873ae5983a98
Boubaker Tayssir <taysir.boubaker2082@gmail.com>
2023-01-10 15:46:55 +0100

    Add icon app to Forms

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 from personal.views import home_screen_view
 from account.views import (
     registration_view,
@@ -47,3 +49,9 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
      name='password_reset_complete'),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
